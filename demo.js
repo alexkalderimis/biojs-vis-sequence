21d898662e4ade2f2edf9e70b1542072df2a54d7
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-10-31 12:40:06 +0000

    Cleaned up the demo script and made it easier to follow.

--- demo.js
+++ demo.js
@@ -1,4 +1,6 @@
-var mySequence;
+window.onload = runDemo;
+
+var component;
 var seq1080 = '1111111111' +
               '2222222222' +
               '3333333333' + 
@@ -36,52 +38,55 @@ var seq1080 = '1111111111' +
               'aacgggatcgtcgtactccgccggtgccgccggcc' +
               'ttttgggcgctctgccctccgccacctgctatacc';
 
-window.onload = function() {		 
+function runDemo () {
+  var evt;
 
-	mySequence = new Sequence({
-		sequence : seq1080,
-		target : "#holder",
-		format : 'CODATA',
-		formatOptions : {
-			title:false,
-			footer:false
-		},
-		id : 'dummy'
-	});
+  component = new Sequence({
+    sequence : seq1080,
+    id : 'dummy',
+    target : document.getElementById('holder'),
+    format : 'FASTA',
+    numCols: 70
+  });
 
-  // DEMO of events - 
-  var evt = Sequence.EVT_ON_SELECTION_CHANGED;
-  mySequence.on(evt, console.log.bind(console, evt));
+  // DEMO of events
+  evt = Sequence.EVT_ON_SELECTION_CHANGED;
+  component.on(evt, console.log.bind(console, evt));
 
-	mySequence.setSelection(81, 87); // Should read GATTACA
+  // How to set the selection.
+  component.setSelection(81, 87); // Should read GATTACA
 
-	mySequence.addAnnotation({
-		name:"track1",
-		html:"<br> Example of <b>HTML</b>",
-		color:"green",
-		regions: [
-		{start: 2, end: 2},
-		{start: 20, end: 30},
-		{start: 31, end: 43},
-			{start: 44, end: 44},
-			{start: 50, end: 90}]
-	});
+  // How to use annotations.
+  component.addAnnotation({
+    name:"track1",
+    html:"<br> Example of <b>HTML</b>",
+    color:"green",
+    regions: [
+      {start: 2, end: 2},
+      {start: 20, end: 30},
+      {start: 31, end: 43},
+      {start: 44, end: 44},
+      {start: 50, end: 90}
+    ]
+  });
 
-	mySequence.addAnnotation({
-		name:"track2",
-		html:"<br> Example of <b>HTML</b>",
-		color:"green",
-		regions: [
-		{start: 7, end: 40},
-		{start: 45, end: 45},
-		{start: 47, end: 48},
-			{start: 49, end: 55},
-			{start: 80, end: 91}]
-	});
+  component.addAnnotation({
+    name:"track2",
+    html:"<br> Example of <b>HTML</b>",
+    color:"green",
+    regions: [
+      {start: 7, end: 40},
+      {start: 45, end: 45},
+      {start: 47, end: 48},
+      {start: 49, end: 55},
+      {start: 80, end: 91}
+    ]
+  });
 
-	//mySequence.setSequence("P");
+  //mySequence.setSequence("P");
 
-	mySequence.addHighlight( { "start": 1, "end": 15, id: 'aaa'} );
-	mySequence.addHighlight( { "start": 7, "end": 21, kind: "bbb" } );
-	mySequence.addHighlight( { "start": 12, "end": 26, "kind": "ccc" } );
-};	    
+  // How to manage highlights.
+  component.addHighlight( { "start": 1, "end": 15, id: 'aaa'} );
+  component.addHighlight( { "start": 7, "end": 21, kind: "bbb" } );
+  component.addHighlight( { "start": 12, "end": 26, "kind": "ccc" } );
+}
